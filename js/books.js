248099ceeaeeb4e7c4881bebff314a5b920567d7
omarroq2 <oliver.marroquin31217@gmail.com>
2026-03-24 20:52:33 -0400

    added the BookStore object with a constructor

---
+++ js/books.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// this is the shared file that all my other pages will use. It will define the data (class and book data)
+
+class BookStore {
+  constructor() {
+    this.books = [];
+  }
+}
