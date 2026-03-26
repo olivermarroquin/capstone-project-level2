d9154a379254cb0877b4ab5fb7b073aa275e72e8
omarroq2 <oliver.marroquin31217@gmail.com>
2026-03-26 17:34:35 -0400

    added the getKey() function to get the API key and confirmed it shows up in the console.

--- js/chat.js
+++ js/chat.js
@@ -26,6 +26,39 @@ function renderMessage(message, isBot) {
   outputEl.appendChild(p);
 }
 
+async function wakeUp() {
+  try {
+    // i put this in here to send a lightweight request to the proxy service.
+    await fetch("https://proxy-key-yquj.onrender.com");
+  } catch (error) {
+    console.error("Wake up request failed.");
+  }
+}
+
+async function getKey() {
+  try {
+    const url = "https://proxy-key-yquj.onrender.com/get-key";
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const res = await fetch(url, options);
+    // console.log(res);
+    if (!res.ok) {
+      throw new Error("Could not get API key");
+    }
+    const data = await res.json();
+    // console.log(data);
+    // console.log(data.key);
+    return data.key;
+  } catch (error) {
+    throw new Error("Could not get secure API Key");
+  }
+}
+// getKey();
+
 function handleSubmit(event) {
   event.preventDefault();
   const userQuestion = promptEl.value.trim();
@@ -34,14 +67,24 @@ function handleSubmit(event) {
     return;
   }
   renderMessage(userQuestion, false);
-  renderMessage("I can help answer questions about our books.", true);
+  //   renderMessage("I can help answer questions about our books.", true);
   promptEl.value = "";
-  renderStatus("Message sent.");
+  renderStatus("Loading...");
 }
 
 function main() {
   renderMessage("Hi! Ask me about our books", true);
   inputAreaEl.addEventListener("submit", handleSubmit);
+
+  // practicing my .then() functions to test the getKey() function.
+  getKey()
+    .then((key) => {
+      console.log("API key received: ", key);
+    })
+    .catch((error) => {
+      console.error(error);
+      //   });
+    });
 }
 
 main();
