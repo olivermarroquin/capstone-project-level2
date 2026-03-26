bff4b39221b835b53c1c41109d1582f4ac30e368
omarroq2 <oliver.marroquin31217@gmail.com>
2026-03-26 12:30:33 -0400

    added the html inputs to my chat.js file and proved that I can render data into those elements. Separated chat messages from the Bot and the User and added a submit handler to prevent the default refresh behavior from the form submit event.

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,36 @@
 "use strict";
+
+// console.log("chat.js loaded");
+
+const outputEl = document.getElementById("output");
+const inputAreaEl = document.getElementById("inputArea");
+const promptEl = document.getElementById("prompt");
+const chatStatusEl = document.getElementById("chatStatus");
+
+console.log(outputEl);
+console.log(inputAreaEl);
+console.log(promptEl);
+console.log(chatStatusEl);
+
+function renderMessage(message, isBot) {
+  const p = document.createElement("p");
+  if (isBot) {
+    p.textContent = `Bot: ${message}`;
+  } else {
+    p.textContent = `You: ${message}`;
+  }
+  outputEl.appendChild(p);
+}
+renderMessage("Hi! Ask me about our books", true);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const userQuestion = promptEl.value.trim();
+  if (!userQuestion) {
+    chatStatusEl.textContent = "Please Enter a Question.";
+    return;
+  }
+  renderMessage(userQuestion, false);
+  promptEl.value = "";
+  chatStatusEl.textContent = "Message sent.";
+}
