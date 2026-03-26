512c7d534c490b22e582b94e6160080952e20b6c
omarroq2 <oliver.marroquin31217@gmail.com>
2026-03-25 21:31:15 -0400

    added all books to the catalog page

--- js/catalog.js
+++ js/catalog.js
@@ -10,11 +10,34 @@ const books = store.getAllBooks();
 
 function renderBookCard(book) {
   const article = document.createElement("article");
+
   const title = document.createElement("h3");
   title.textContent = book.title;
-  article.append(title);
+
+  const author = document.createElement("p");
+  author.textContent = `Author: ${book.author}`;
+
+  const genre = document.createElement("p");
+  genre.textContent = `Genre: ${book.genre}`;
+
+  const price = document.createElement("p");
+  price.textContent = `Price: $${book.price}`;
+
+  const stock = document.createElement("p");
+  stock.textContent = `In Stock: ${book.stock}`;
+
+  const description = document.createElement("p");
+  description.textContent = book.description;
+
+  article.append(title, author, genre, price, stock, description);
   return article;
 }
 
-const firstBookCard = renderBookCard(books[0]);
-catalogBooksEl.appendChild(firstBookCard);
+// const firstBookCard = renderBookCard(books[0]);
+// catalogBooksEl.appendChild(firstBookCard);
+
+catalogBooksEl.textContent = "";
+books.forEach((book) => {
+  const card = renderBookCard(book);
+  catalogBooksEl.appendChild(card);
+});
