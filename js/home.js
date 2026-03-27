dc7ae2d10fb76d152293cd6d05f6bc283eb25938
omarroq2 <oliver.marroquin31217@gmail.com>
2026-03-26 20:55:17 -0400

    finished home page styling for now.

--- js/home.js
+++ js/home.js
@@ -19,25 +19,32 @@ const featuredBooks = store.getFeaturedBooks();
 function renderBookCard(book) {
   // I used createElement and textContent to safely construct DOM elements and avoid potential injection issues. It also makes the structure clearer and easier to debug compared to inserting HTML strings.
   const article = document.createElement("article");
-  article.className = "bg-white rounded-xl";
+  article.className =
+    "bg-white rounded-xl shadow-md p-4 hover:shadow-xl transition";
 
   const title = document.createElement("h3");
   title.textContent = book.title;
+  title.className = "text-sm text-gray-600";
 
   const author = document.createElement("p");
   author.textContent = `Author: ${book.author}`;
+  author.className = "text-sm text-gray-600";
 
   const genre = document.createElement("p");
   genre.textContent = `Genre: ${book.genre}`;
+  genre.className = "text-sm text-gray-600";
 
   const price = document.createElement("p");
   price.textContent = `Price: $${book.price}`;
+  price.className = "text-sm text-gray-800 font-medium";
 
   const stock = document.createElement("p");
   stock.textContent = `In Stock: ${book.stock}`;
+  stock.className = "text-sm text-gray-500";
 
   const description = document.createElement("p");
   description.textContent = book.description;
+  description.className = "text-sm mt-2";
 
   // I used .append() because I can add multiple elements at once instead of multiple appendChild()'s
   article.append(title, author, genre, price, stock, description);
